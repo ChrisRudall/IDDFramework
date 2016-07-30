22610fcf0bbccc13f9f1f4b55a2402515f958924
Chris <chrisrudall@Christians-MBP.home>
2016-07-30 20:27:48 +0100

    room

--- src/InDemandDigital/IDDFramework/Entities/Room.php
+++ src/InDemandDigital/IDDFramework/Entities/Room.php
@@ -15,14 +15,15 @@ class Room extends Entity{
         // Debug::niceprint("getPerformancesForRoomID");
         $sql = "SELECT * FROM `performances` WHERE `room_id`=$this->id ORDER BY `start_time`";
         $result = IDD\Database::query($sql);
-        return self::convertResultSetToObjectArray($result,'InDemandDigital\Entities\Performance');
+        return self::convertResultSetToObjectArray($result,'InDemandDigital\IDDFramework\Entities\Performance');
     }
     public function getPerformancesByDisplayOrder($limit = NULL){
 
         $sql = "SELECT * FROM `performances` WHERE `room_id`=$this->id ORDER BY `display_order` LIMIT $limit";
             Debug::niceprint($sql);
         $rs = IDD\Database::query($sql);
-        while ($r = $rs->fetch_object('InDemandDigital\Entities\Performance')){
+        while ($r = $rs->fetch_object('InDemandDigital\IDDFramework\Entities\Performance')){
+            $r->artist = new Artist($r->artist_id);
             $a [] = $r;
         }
         return $a;
