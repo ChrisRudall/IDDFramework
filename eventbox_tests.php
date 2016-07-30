58c6c2b0a305a925cb76b6e338e6c9de6166d16d
Chris <chrisrudall@Christians-MBP.home>
2016-07-30 17:06:50 +0100

    added eventbox

---
+++ eventbox_tests.php
@@ -0,0 +1,14 @@
+<?php namespace InDemandDigital;
+use \InDemandDigital\Eventbox\eventbox;
+ ?>
+
+ <!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+<?php new eventbox; ?>
+
+    </body>
+</html>
--- src/InDemandDigital/IDDFramework/SocialManager.php
+++ src/InDemandDigital/IDDFramework/SocialManager.php
@@ -9,5 +9,14 @@ class SocialManager{
     function getAccount($name){
         return new Ent\SocialAccount($name);
     }
+    function getPostsForAccount($account){
+        if($account->include_any == 1){
+            $sql = "SELECT * FROM `data` WHERE `publish_date`<NOW() AND `expires`>NOW() AND `publish`='1' AND(`account`='$account->account_name' OR `account`='any')";
+        }
+        else{
+            $sql = "SELECT * FROM `data` WHERE `publish_date`<NOW() AND `expires`>NOW() AND `publish`='1' AND `account`='$account->account_name'";
+        }
+        return Database::query($sql);
+    }
 }
 ?>
