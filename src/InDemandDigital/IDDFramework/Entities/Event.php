ffc07cc17c6c833ead2089df48fe150f941cd6de
Chris <chrisrudall@Christians-MBP.home>
2016-07-30 19:01:27 +0100

    get rooms for event

--- src/InDemandDigital/IDDFramework/Entities/Event.php
+++ src/InDemandDigital/IDDFramework/Entities/Event.php
@@ -3,6 +3,22 @@ namespace InDemandDigital\IDDFramework\Entities;
 use InDemandDigital\IDDFramework as IDD;
 
 class Event extends Entity{
+    function getAllRooms(){
+        $sql = "SELECT * FROM rooms WHERE event='$this->id' ORDER BY start_time";
+        $rs = IDD\Database::query($sql);
+        while ($r = $rs->fetch_object('\InDemandDigital\IDDFramework\Entities\Event')) {
+            $array [] = $r;
+        }
+        return $array;
 
+    }
+    function getAllFutureRooms(){
+        $sql = "SELECT * FROM rooms WHERE event='$this->id' AND `start_time`>NOW() ORDER BY start_time";
+        $rs = IDD\Database::query($sql);
+        while ($r = $rs->fetch_object('\InDemandDigital\IDDFramework\Entities\Event')) {
+            $array [] = $r;
+        }
+        return $array;
+    }
 }
 ?>
