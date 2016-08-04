e4e618c25363e066c3ebbcad3a4841d996063a83
Chris <chrisrudall@Chriss-MBP.home>
2016-08-04 10:41:32 +0100

    custom defuse

--- tests/subscribe_tests.php
+++ tests/subscribe_tests.php
@@ -8,16 +8,48 @@ require '../vendor/autoload.php';
 use \InDemandDigital\IDDFramework\Entities AS Ent;
 use \InDemandDigital\IDDFramework AS IDD;
 use \InDemandDigital\IDDFramework\Tests\Debug AS Debug;
-use Defuse\Crypto\Crypto;
-use Defuse\Crypto\Key;
+use \InDemandDigital\IDDFramework\Crypto;
+use \InDemandDigital\IDDFramework\Crypto\Exception as Ex;
 
 
 // $vars = ['email' => 'steve@indemandmusic.com'];
-var_dump(phpversion());
 // var_dump( IDD\Mail::addSubscriber($vars));
 $d = 'hello';
-$key = Key::createNewRandomKey();
-$e = Crypto::encrypt($d,$key);
-echo Crypto::decrypt($e,$key);
+// $e = Crypto::encrypt($d,$key);
+// echo Crypto::decrypt($e,$key);
 
+// require_once('../src/defuse_v1/php-encryption/autoload.php');
+  try {
+      $key = Crypto\Crypto::CreateNewRandomKey();
+      // WARNING: Do NOT encode $key with bin2hex() or base64_encode(),
+      // they may leak the key to the attacker through side channels.
+  } catch (CryptoTestFailedException $ex) {
+      die('Cannot safely create a key');
+  } catch (CannotPerformOperationException $ex) {
+      die('Cannot safely create a key');
+  }
+
+  $message = "ATTACK AT DAWN";
+  try {
+      $ciphertext = Crypto\Crypto::Encrypt($message, $key);
+  } catch (CryptoTestFailedException $ex) {
+      die('Cannot safely perform encryption');
+  } catch (CannotPerformOperationException $ex) {
+      die('Cannot safely perform decryption');
+  }
+
+  try {
+      echo $decrypted = Crypto\Crypto::Decrypt($ciphertext, $key);
+  } catch (InvalidCiphertextException $ex) { // VERY IMPORTANT
+      // Either:
+      //   1. The ciphertext was modified by the attacker,
+      //   2. The key is wrong, or
+      //   3. $ciphertext is not a valid ciphertext or was corrupted.
+      // Assume the worst.
+      die('DANGER! DANGER! The ciphertext has been tampered with!');
+  } catch (CryptoTestFailedException $ex) {
+      die('Cannot safely perform encryption');
+  } catch (CannotPerformOperationException $ex) {
+      die('Cannot safely perform decryption');
+  }
  ?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInitf6d89f8e667bfb35a87876e84dfe07fa
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitf6d89f8e667bfb35a87876e84dfe07fa::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequiref6d89f8e667bfb35a87876e84dfe07fa($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequiref6d89f8e667bfb35a87876e84dfe07fa($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
