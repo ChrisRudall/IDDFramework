907cc02ddcb914384d23d9ce90cb179cb55fdded
Chris <chrisrudall@Christians-MBP.home>
2016-08-08 20:40:21 +0100

    added create staffmamber

--- src/InDemandDigital/IDDFramework/Entities/Person.php
+++ src/InDemandDigital/IDDFramework/Entities/Person.php
@@ -3,9 +3,21 @@ namespace InDemandDigital\IDDFramework\Entities;
 use InDemandDigital\IDDFramework as IDD;
 
 class Person extends Entity{
+
+    public function __construct(){
+        trigger_error("Please use custom function to create a person",E_USER_ERROR);
+    }
+
     public static function getPersonForUUID($uuid){
         IDD\Database::connectToMailingList();
         $sql = "SELECT * FROM `public` WHERE `uuid`='$uuid'";
+        $rs = IDD\Database::query($sql);
+        return $rs->fetch_object('\InDemandDigital\IDDFramework\Entities\Person');
+    }
+
+    public static function getStaffMemberWithID($id){
+        // IDD\Database::connectToMailingList();
+        $sql = "SELECT * FROM `staff` WHERE `id`='$id'";
         $rs = IDD\Database::query($sql);
         return $rs->fetch_object('\InDemandDigital\IDDFramework\Entities\Person');
     }
---
+++ tests/mailer_tests.php
@@ -0,0 +1,16 @@
+<?php
+namespace InDemandDigital;
+session_start();
+set_include_path('includes');
+Date_default_timezone_set('UTC');
+require '../vendor/autoload.php';
+
+use \InDemandDigital\IDDFramework\Entities AS Ent;
+use \InDemandDigital\IDDFramework AS IDD;
+use \InDemandDigital\IDDFramework\Tests\Debug AS Debug;
+use \InDemandDigital\IDDFramework\Crypto;
+use \InDemandDigital\IDDFramework\Crypto\Exception as Ex;
+
+Header ("Location: mailto:chris@indemandmusic.com")
+
+ ?>
