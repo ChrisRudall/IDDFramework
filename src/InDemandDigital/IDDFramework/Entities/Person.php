e94818260fc5f70f06d85616f62366fb10016d10
Chris <chrisrudall@Christians-MBP.home>
2016-08-08 20:46:14 +0100

    c

--- src/InDemandDigital/IDDFramework/Entities/Person.php
+++ src/InDemandDigital/IDDFramework/Entities/Person.php
@@ -4,8 +4,10 @@ use InDemandDigital\IDDFramework as IDD;
 
 class Person extends Entity{
 
-    public function __construct(){
-        trigger_error("Please use custom function to create a person",E_USER_ERROR);
+    public function __construct($i){
+        if($i){
+            trigger_error("Please use custom function to create a person",E_USER_ERROR);
+        }
     }
 
     public static function getPersonForUUID($uuid){
